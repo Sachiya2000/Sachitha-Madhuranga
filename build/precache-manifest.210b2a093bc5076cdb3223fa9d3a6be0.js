dc44d1983fa21b919a6cca41d6b13f189e258405
Sachitha Madhuranga <118953518+Sachiya2000@users.noreply.github.com>
2024-07-04 01:33:51 +0530

    package updated

---
+++ build/precache-manifest.210b2a093bc5076cdb3223fa9d3a6be0.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c7aeccd1c0d0658f2653e5f30ac6d6f",
+    "url": "/Sachiya2000.github.io/index.html"
+  },
+  {
+    "revision": "92ede9c6322bb251d54f",
+    "url": "/Sachiya2000.github.io/static/css/2.80f479d8.chunk.css"
+  },
+  {
+    "revision": "303b50dae18f81377d17",
+    "url": "/Sachiya2000.github.io/static/css/main.45d95223.chunk.css"
+  },
+  {
+    "revision": "92ede9c6322bb251d54f",
+    "url": "/Sachiya2000.github.io/static/js/2.6e24813f.chunk.js"
+  },
+  {
+    "revision": "35f1a3ca561e93b64bcba705f76bd30f",
+    "url": "/Sachiya2000.github.io/static/js/2.6e24813f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "303b50dae18f81377d17",
+    "url": "/Sachiya2000.github.io/static/js/main.ca567cf9.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/Sachiya2000.github.io/static/js/main.ca567cf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02d208994a99345d2384",
+    "url": "/Sachiya2000.github.io/static/js/runtime-main.38b8f23b.js"
+  },
+  {
+    "revision": "d584f24224d90671bed4403c31fdcf96",
+    "url": "/Sachiya2000.github.io/static/media/Sachiya.d584f242.jpg"
+  },
+  {
+    "revision": "8c1ae41d4ad287a9404c619ad29aa630",
+    "url": "/Sachiya2000.github.io/static/media/css.8c1ae41d.png"
+  },
+  {
+    "revision": "b931dd24f43764b277fa1cd4d29c241d",
+    "url": "/Sachiya2000.github.io/static/media/img2.b931dd24.jpg"
+  },
+  {
+    "revision": "b931dd24f43764b277fa1cd4d29c241d",
+    "url": "/Sachiya2000.github.io/static/media/imgbackground.b931dd24.jpg"
+  },
+  {
+    "revision": "b60066c7b0564b932b031df9ef539af8",
+    "url": "/Sachiya2000.github.io/static/media/java.b60066c7.jpg"
+  },
+  {
+    "revision": "e27eba9ab035d5a99877059bc3893420",
+    "url": "/Sachiya2000.github.io/static/media/java2.e27eba9a.png"
+  },
+  {
+    "revision": "0fd5adbe1c510c7bb6b1dc93166a9b63",
+    "url": "/Sachiya2000.github.io/static/media/oop.0fd5adbe.png"
+  },
+  {
+    "revision": "6c518be2a201955984e3250b73b45d67",
+    "url": "/Sachiya2000.github.io/static/media/pdf1.6c518be2.png"
+  },
+  {
+    "revision": "5f9ca06c5ca285e2b102fb8ae765a893",
+    "url": "/Sachiya2000.github.io/static/media/py.5f9ca06c.jpg"
+  },
+  {
+    "revision": "8c1ef40db08016674253229016ea3000",
+    "url": "/Sachiya2000.github.io/static/media/react.8c1ef40d.jpg"
+  },
+  {
+    "revision": "f7f40b4002c9132b20017986b6754b71",
+    "url": "/Sachiya2000.github.io/static/media/react_basic certificate.f7f40b40.png"
+  },
+  {
+    "revision": "dcff966abddb56e037a628d7d147c6b4",
+    "url": "/Sachiya2000.github.io/static/media/software_engineer_intern certificate.dcff966a.png"
+  },
+  {
+    "revision": "fb39b7d1a55ada9683026d319dc13a31",
+    "url": "/Sachiya2000.github.io/static/media/web.fb39b7d1.jpg"
+  },
+  {
+    "revision": "606f0cd72da15bb2aa6d4008f01fa618",
+    "url": "/Sachiya2000.github.io/static/media/webdevelopment.606f0cd7.png"
+  }
+]);
